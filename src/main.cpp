0f7f79898e4580392ebfada46bb5c546ec68053e
Tobias Fella <fella@posteo.de>
2020-10-08 20:52:31 +0200

    Make clazy happier

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,18 @@
  * SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
  */
 
-#ifdef Q_OS_ANDROID
-#include <QGuiApplication>
-#else
-#include <QApplication>
-#endif
 
 #include <QCommandLineParser>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickView>
 #include <QQuickStyle>
+
+#ifdef Q_OS_ANDROID
+#include <QGuiApplication>
+#else
+#include <QApplication>
+#endif
 
 #include <KAboutData>
 #include <KLocalizedContext>
