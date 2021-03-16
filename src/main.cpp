c04f38acc057995c70f7102cfdd4a353ed336fbb
Tobias Fella <fella@posteo.de>
2021-03-16 15:55:01 +0100

    Remove unrelated change

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,6 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 #endif
 
-    qDebug() << QQuickStyle::availableStyles();
-
     QCoreApplication::setOrganizationName(QStringLiteral("KDE"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("kde.org"));
     QCoreApplication::setApplicationName(QStringLiteral("Alligator"));
