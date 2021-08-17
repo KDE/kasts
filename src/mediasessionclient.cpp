d00d2ea1b190c9bed798857d05bb93125cd7c674
Swapnil Tripathi <swapnil06.st@gmail.com>
2021-08-18 02:38:30 +0530

    License fix.

--- src/mediasessionclient.cpp
+++ src/mediasessionclient.cpp
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2021 Swapnil Tripathi <swapnil06.st@gmail.com>
- * SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+ * SPDX-License-Identifier: LicenseRef-KDE-Accepted-LGPL
 */
 
 #include "mediasessionclient.h"
