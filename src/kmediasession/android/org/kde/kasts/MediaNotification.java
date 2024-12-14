041e2c352ea3260fff9ddac346a452733a153f0b
Tobias Fella <fella@posteo.de>
2024-12-14 19:37:44 +0100

    Wtf

--- src/kmediasession/android/org/kde/kasts/MediaNotification.java
+++ src/kmediasession/android/org/kde/kasts/MediaNotification.java
@@ -2,12 +2,24 @@ package org.kde.kasts;
 
 import android.util.Log;
 import androidx.media3.common.SimpleBasePlayer;
+import androidx.media3.common.Player;
 
 class Receiver extends BroadcastReceiver {
 
 }
 
 class KastsPlayer extends SimpleBasePlayer {
+    @Override
+    public SimpleBasePlayer.State getState() {
+        SimpleBasePlayer.State.Builder builder = new SimpleBasePlayer.State.Builder();
+
+        Player.Commands.Builder commandsBuilder = new Player.Commands.Builder();
+        commandsBuilder.add(Player.COMMAND_PLAY_PAUSE);
+        builder.setAvailableCommands(commandsBuilder.build());
+        builder.setContentPositionMs(1234);
+        builder.setPlaybackState(Player.State.STATE_READY);
+        return builder.build();
+    }
 }
 
 public class MediaNotification {
