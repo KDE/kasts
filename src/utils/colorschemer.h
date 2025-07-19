66e8dd11c00a1c1dcd7c48b0845413cf652dfb9f
Nicolas Fella <nicolas.fella@gmx.de>
2025-07-19 21:04:22 +0200

    Clean up color scheme handling
    KColorSchemeManager saves the setting on its own, no need to do that ourselves
    
    Copy the approach Neochat uses

--- src/utils/colorschemer.h
+++ src/utils/colorschemer.h
@@ -1,44 +1,52 @@
-/**
- * SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
- * SPDX-FileCopyrightText: 2023 Bart De Vries <bart@mogwai.be>
- * SPDX-License-Identifier: LGPL-2.1-or-later
- */
+// SPDX-FileCopyrightText: 2021 Carl Schwan <carlschwan@kde.org>
+// SPDX-License-Identifier: LGPL-2.1-or-later
 
 #pragma once
 
 #include <QObject>
 #include <QQmlEngine>
-#include <QtQml/qqmlregistration.h>
 
 class QAbstractItemModel;
 class KColorSchemeManager;
 
+/**
+ * @class ColorSchemer
+ *
+ * A class to provide a wrapper around KColorSchemeManager to make it available in
+ * QML.
+ *
+ * @sa KColorSchemeManager
+ */
 class ColorSchemer : public QObject
 {
     Q_OBJECT
     QML_ELEMENT
     QML_SINGLETON
 
+    /**
+     * @brief A QAbstractItemModel of all available color schemes.
+     *
+     * @sa QAbstractItemModel
+     */
     Q_PROPERTY(QAbstractItemModel *model READ model CONSTANT)
 
 public:
-    static ColorSchemer *create(QQmlEngine *, QJSEngine *)
-    {
-        auto inst = &instance();
-        QJSEngine::setObjectOwnership(inst, QJSEngine::ObjectOwnership::CppOwnership);
-        return inst;
-    }
-
     explicit ColorSchemer(QObject *parent = nullptr);
-
-    static ColorSchemer &instance();
+    ~ColorSchemer();
 
     QAbstractItemModel *model() const;
-    Q_INVOKABLE void apply(int idx);
-    Q_INVOKABLE void apply(const QString &name);
-    Q_INVOKABLE int indexForScheme(const QString &name) const;
-    Q_INVOKABLE QString nameForIndex(int index) const;
 
-private:
-    KColorSchemeManager *c;
+    /**
+     * @brief Activates the KColorScheme identified by the provided index.
+     *
+     * @sa KColorScheme
+     */
+    Q_INVOKABLE void apply(int idx);
+
+    /**
+     * @brief Get the row for the current color scheme.
+     *
+     * @sa KColorScheme
+     */
+    Q_INVOKABLE int indexForCurrentScheme();
 };
